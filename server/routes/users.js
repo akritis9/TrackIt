ff85f2de4207b4da8cd668e295bc087210b07e9b
akritis9 <9akriti@gmail.com>
2022-05-24 10:59:52 -0700

    added API endpoint handler for exercises

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+/* first endpoint */
+/* call users.find() to get a list of all the users from the database */
 router.route('/').get((req, res) => {
     User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+/* second endpoint */
+/* handles incoming HTTP POST requests on the /users/add/ URL path */
 router.route('/add').post((req, res) => {
     const username = req.body.username;
 
