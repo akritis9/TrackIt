c421d3e05fa692ba05c0513455c1aae6346d5642
akritis9 <9akriti@gmail.com>
2022-05-25 10:00:07 -0700

    updated navbar syntax for react-router-dom v6.3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import './App.css';
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 import Navbar from "./components/navbar.component"
-import ExercisesList from "./components/exercise-list.component";
+import ExerciseList from "./components/exercise-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
@@ -14,11 +16,12 @@ function App() {
     <Router>
       <div className="container">
         <Navbar />
-        <br/>
-        <Route path="/" exact component={ExercisesList} />
-        <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={CreateExercise} />
-        <Route path="/user" component={CreateUser} />
+        <Routes>
+        <Route path="/" element={<ExerciseList />} />
+        <Route path="/edit/:id" element={<EditExercise />} />
+        <Route path="/create" element={<CreateExercise />} />
+        <Route path="/user" element={<CreateUser />} />
+        </Routes>
       </div>
     </Router>
   );
