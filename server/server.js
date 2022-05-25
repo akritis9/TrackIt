81147baa06d88315e84e810ed6eee61591796b63
akritis9 <9akriti@gmail.com>
2022-05-25 07:30:17 -0700

    deleted files

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');*/
 
 require('dotenv').config();
 
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+/* const uri = process.env.ATLAS_URI;
 
 mongoose.connect(uri);
 
@@ -20,15 +20,15 @@ const connection = mongoose.connection;
 
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
-})
+}) */
 
 /*load the routers from other files */
-const exercisesRouter = require('./routes/exercises');
-const userRouter = require('./routes/users');
+/*const exercisesRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');*/
 
 /* add routers as middleware */
-app.use('/exercises', exercisesRouter);
-app.use('/users', userRouter);
+/*app.use('/exercises', exercisesRouter);
+app.use('/users', userRouter);*/
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
