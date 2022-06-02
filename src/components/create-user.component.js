4c491199555b3772fe2fc3f4c986a2006f311a07
akritis9 <9akriti@gmail.com>
2022-06-02 11:26:08 -0700

    connected front end and back end

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -12,6 +13,7 @@ export default class CreateUser extends Component {
     };
   }
 
+
   onChangeUsername(e) {
     this.setState({
       username: e.target.value
@@ -23,6 +25,9 @@ export default class CreateUser extends Component {
       username: this.state.username,
     };
     console.log(newUser);
+
+    // save data to database
+    axios.post('http://localhost:5000/users/add',newUser).then(res => console.log(res.data))
     
     this.setState({
       username: ''
